d0bca08db559aafadd71b7efa424539d332af35c
marifm1986 <marifm1986@gmail.com>
2026-05-12 23:56:35 +0600

    ui fixes

--- app/context/contactInfo.ts
+++ app/context/contactInfo.ts
@@ -1,4 +1,4 @@
-const contactInfo = {
+export const contactInfo = {
     email: "info@masjidulsalam.org",
     phone: "+৮৮০ ১৭৩৮৭৬৭৪৯৩",
     address: "পিরেরচর পূর্বপাড়া, পিরেরচর, ভাঙ্গা, ফরিদপুর",
